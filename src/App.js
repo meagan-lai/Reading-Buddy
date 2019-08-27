89d568a52298912981bf544a1527575c2f3342d9
Meagan Lai <laimeagan9@gmail.com>
2019-08-27 00:51:10 -0400

    styling changes

--- src/App.js
+++ src/App.js
@@ -365,6 +365,98 @@ class App extends Component {
               </div>*/}
           </div>
         )}
+        
+
+        {this.state.value === 1 && (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              backgroundColor: "#8CBEBC",
+              margin: 20
+            }}
+          >
+            <List
+              component="nav"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center"
+              }}
+            >
+              <ListItem button>
+                <Icon fontSize="large">short_text</Icon>
+                <div style={{ fontSize: 24 }}>
+                  Number of words attempted: 89
+                </div>
+              </ListItem>
+              <ListItem button>
+                <Icon fontSize="large" style={{ color: "green" }}>
+                  check
+                </Icon>
+
+                <div style={{ fontSize: 24 }}>
+                  Number of words correctly read: 80
+                </div>
+              </ListItem>
+              <ListItem button>
+                <Icon fontSize="large" style={{ color: "red" }}>
+                  close
+                </Icon>
+
+                <div style={{ fontSize: 24 }}>
+                  Number of words incorrectly read: 9
+                </div>
+              </ListItem>
+            </List>
+            <RadialChart
+              width={400}
+              height={400}
+              stroke="black"
+              ç
+              data={[
+                { angle: 89, label: "Words Read" },
+                { angle: 80, color: "rgb(95, 204, 113)", label: "Correct" },
+                { angle: 9, color: "rgb(255, 0, 0)", label: "Incorrect" }
+              ]}
+            />
+            <XYPlot strokeWidth={4} width={500} height={500}>
+              <LineSeries
+                data={[
+                  { x: 1, y: 5 },
+                  { x: 2, y: 6 },
+                  { x: 3, y: 5 },
+                  { x: 4, y: 8 },
+                  { x: 5, y: 7 },
+                  { x: 6, y: 10 },
+                  { x: 7, y: 12 },
+                  { x: 8, y: 14 },
+                  { x: 9, y: 14 },
+                  { x: 10, y: 13 },
+                  { x: 11, y: 16 },
+                  { x: 12, y: 15 },
+                  { x: 13, y: 16 },
+                  { x: 14, y: 16 }
+                ]}
+              />
+              <XAxis
+                style={{
+                  line: { strokeWidth: 2, stroke: "black" },
+                  text: { fontWeight: 700 }
+                }}
+                title="Days"
+              />
+              <YAxis
+                style={{
+                  line: { strokeWidth: 2, stroke: "black" },
+                  text: { fontWeight: 700 }
+                }}
+                title="Attempts-Errors Ratio"
+              />
+            </XYPlot>
+          </div>
+        )}
         <div
           style={{
             display: "flex",
@@ -466,97 +558,6 @@ class App extends Component {
             E
           </Typography>
         </div>
-
-        {this.state.value === 1 && (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              backgroundColor: "#b7def7",
-              margin: 20
-            }}
-          >
-            <List
-              component="nav"
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center"
-              }}
-            >
-              <ListItem button>
-                <Icon fontSize="large">short_text</Icon>
-                <div style={{ fontSize: 24 }}>
-                  Number of words attempted: 89
-                </div>
-              </ListItem>
-              <ListItem button>
-                <Icon fontSize="large" style={{ color: "green" }}>
-                  check
-                </Icon>
-
-                <div style={{ fontSize: 24 }}>
-                  Number of words correctly read: 80
-                </div>
-              </ListItem>
-              <ListItem button>
-                <Icon fontSize="large" style={{ color: "red" }}>
-                  close
-                </Icon>
-
-                <div style={{ fontSize: 24 }}>
-                  Number of words incorrectly read: 9
-                </div>
-              </ListItem>
-            </List>
-            <RadialChart
-              width={400}
-              height={400}
-              stroke="black"
-              ç
-              data={[
-                { angle: 89, label: "Words Read" },
-                { angle: 80, color: "rgb(95, 204, 113)", label: "Correct" },
-                { angle: 9, color: "rgb(255, 0, 0)", label: "Incorrect" }
-              ]}
-            />
-            <XYPlot strokeWidth={4} width={500} height={500}>
-              <LineSeries
-                data={[
-                  { x: 1, y: 5 },
-                  { x: 2, y: 6 },
-                  { x: 3, y: 5 },
-                  { x: 4, y: 8 },
-                  { x: 5, y: 7 },
-                  { x: 6, y: 10 },
-                  { x: 7, y: 12 },
-                  { x: 8, y: 14 },
-                  { x: 9, y: 14 },
-                  { x: 10, y: 13 },
-                  { x: 11, y: 16 },
-                  { x: 12, y: 15 },
-                  { x: 13, y: 16 },
-                  { x: 14, y: 16 }
-                ]}
-              />
-              <XAxis
-                style={{
-                  line: { strokeWidth: 2, stroke: "black" },
-                  text: { fontWeight: 700 }
-                }}
-                title="Days"
-              />
-              <YAxis
-                style={{
-                  line: { strokeWidth: 2, stroke: "black" },
-                  text: { fontWeight: 700 }
-                }}
-                title="Attempts-Errors Ratio"
-              />
-            </XYPlot>
-          </div>
-        )}
       </div>
     );
   }
